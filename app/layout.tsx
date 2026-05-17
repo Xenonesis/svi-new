b69164ba045a2140afc6e976a2775a5566e65bf7
Xenonesis <aaktaditya09@gmail.com>
2026-05-17 10:25:57 +0530

    Update layout and Next.js types

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata, Viewport } from 'next';
 import { Inter, Playfair_Display } from 'next/font/google';
-import Script from 'next/script';
 import './globals.css';
 import ClientProviders from '@/src/components/ClientProviders';
 
@@ -59,9 +58,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script
-          id="theme-init"
-          strategy="beforeInteractive"
+        <script
           dangerouslySetInnerHTML={{ __html: THEME_SCRIPT }}
         />
         <script
