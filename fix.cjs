b3aa9c8b859a1f3d0f57b761778bc595fedce9d6
Xenonesis <aaktaditya09@gmail.com>
2026-06-21 17:59:27 +0530

    chore: allotment pages, document generator, cleanup

--- fix.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const files = ['app/admin/lottery/page.tsx', 'src/components/lottery/LotteryDrawSection.tsx'];
-
-files.forEach(f => {
-  let content = fs.readFileSync(f, 'utf8');
-  content = content.replace(/\\\`/g, '`').replace(/\\\$/g, '$');
-  fs.writeFileSync(f, content);
-});
-console.log('Fixed files');
